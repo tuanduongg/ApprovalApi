ce63e7cc2825257b110d862c141b1a2603539ade
tuanduongg <tuanmixmusic@gmail.com>
2024-07-25 05:52:21 +0700

    commit 25-7

--- src/database/configDB.ts
+++ src/database/configDB.ts
@@ -1,8 +1,11 @@
 export const ConfigDB = {
-    host: 'localhost', //security
+    // host: '113.160.154.39', //deploy
+    host: 'localhost', //office
     // host: 'TUANIT-PC\\SQLEXPRESS', //office
     port: 1433,
+    // port: 4343, //deploy
     username: `sa`,
+    // password: 'JHcos@123@123', //deplay
     password: '1234',
     database: 'RND',
 }
