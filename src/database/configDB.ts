af4b7ee91e65198b0e53fd97e48570553f437b08
Dương Tuấn <69351028+tuanduongg@users.noreply.github.com>
2025-07-27 23:22:57 +0700

    Update configDB.ts

--- src/database/configDB.ts
+++ src/database/configDB.ts
@@ -2,7 +2,7 @@ export const ConfigDB = {
 	host: process.env.HOST_DB || 'hnseowonintech.io.vn',
     port: process.env.PORT_DB ? parseInt(process.env.PORT_DB) : 4343,
     username: `sa`,
-    password: 'JHcos@123@123',
+    password: '',
 	
   // host: 'TUANIT-PC\\SQLEXPRESS', //office
   // port: 1433,
